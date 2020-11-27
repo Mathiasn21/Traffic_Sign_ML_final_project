3d72ffbb512464249df488421b735cda862b3762
Mathiasn21 <Mathiasn21@users.noreply.github.com>
2020-11-27 16:21:59 +0100

    Update main_cnn.py

--- main/main_cnn.py
+++ main/main_cnn.py
@@ -5,7 +5,8 @@ from ml_models.cnn.layer.dense import Dense
 
 training_images = mnist.test_images()[:1000]
 training_labels = mnist.test_labels()[:1000]
+num_labels = 10
 
 cnn = CNN(1, training_images, training_labels)
-cnn.add(Dense(10, 'softmax'))
+cnn.add(Dense(num_labels, 'softmax'))
 cnn.fit()
