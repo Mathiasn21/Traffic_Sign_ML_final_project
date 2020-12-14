5f512e5e448461b22e237ff0261382a54e1f5f0d
Mathiasn21 <Mathiasn21@users.noreply.github.com>
2020-12-14 10:53:01 +0100

    Update svm_test.py

--- main/source/svm_test.py
+++ main/source/svm_test.py
@@ -1,6 +1,9 @@
 from matplotlib import pyplot as plt
 import numpy as np
+from matplotlib.colors import ListedColormap
 from numpy.core.multiarray import ndarray
+from sklearn import datasets
+from sklearn.datasets import make_blobs, make_circles
 
 
 class SVM:
@@ -17,13 +20,14 @@ class SVM:
 
     def fit(self, X, y):
         self.X = X.copy()
-        self.y = y * 2 - 1
+        self.y = y * 2 - 1 # shifts the label center from 0,1 to -1,1
         self.lambdas = np.zeros_like(self.y, dtype=float)
         self.K = self.kernel(self.X, self.X) * self.y[:, np.newaxis] * self.y
 
         for _ in range(self.max_iter):
             for idxM in range(len(self.lambdas)):
                 idxL = np.random.randint(0, len(self.lambdas))
+
                 Q = self.K[[[idxM, idxM], [idxL, idxL]], [[idxM, idxL], [idxM, idxL]]]
                 v0 = self.lambdas[[idxM, idxL]]
                 k0 = 1 - np.sum(self.lambdas * self.K[[idxM, idxL]], axis=1)
@@ -50,13 +54,26 @@ def rbf(gamma, feature_vectors: ndarray, weights: ndarray):
     return classifiers
 
 
-number = 100
-x_features = np.random.randn(number)
+X, y = make_blobs(n_samples=50, centers=2, random_state=0, cluster_std=1.4)
+X, y = make_circles(100, factor=.1, noise=.1)
 
-y_labels = np.random.randn(number)
 
-x_features = rbf(1, x_features, weights=np.ones(x_features.shape[0]))
+def test_plot(X, y, svm_model, axes, title):
+    plt.axes(axes)
+    xlim = [np.min(X[:, 0]), np.max(X[:, 0])]
+    ylim = [np.min(X[:, 1]), np.max(X[:, 1])]
+    xx, yy = np.meshgrid(np.linspace(*xlim, num=700), np.linspace(*ylim, num=700))
+    rgb = np.array([[210, 0, 0], [0, 0, 150]]) / 255.0
 
-plt.plot(x_features, y_labels, 'ro')
-plt.ylabel('some numbers')
-plt.show()
+    svm_model.fit(X, y)
+    z_model = svm_model.decision_function(np.c_[xx.ravel(), yy.ravel()]).reshape(xx.shape)
+
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+    plt.contour(xx, yy, z_model, colors='k', levels=[-1, 0, 1], alpha=0.5, linestyles=['--', '-', '--'])
+    plt.contourf(xx, yy, np.sign(z_model.reshape(xx.shape)), alpha=0.3, levels=2, cmap=ListedColormap(rgb), zorder=1)
+    plt.title(title)
+    plt.show()
+
+
+fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(12, 4))
+test_plot(X, y, SVM(kernel='rbf', C=10, max_iter=60, degree=3, gamma=1), axs[0], 'OUR ALGORITHM')
