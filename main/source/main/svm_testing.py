1313d48f9b6878a4ee9d41985596efae9a6ddb14
Mathiasn21 <Mathiasn21@users.noreply.github.com>
2020-12-17 11:10:14 +0100

    Update svm_testing.py

--- main/source/main/svm_testing.py
+++ main/source/main/svm_testing.py
@@ -12,12 +12,12 @@ x_features = x_features.reshape((n, -1))
 
 x_train, x_test, y_train, y_test = train_test_split(x_features, y_targets, test_size=0.2)
 
-best_svc = SVC(C=2.5, gamma=0.1, kernel='rbf', verbose=True)
+best_svc = SVC(C=2.1, gamma=0.00001, kernel='rbf', verbose=True)
 best_svc.fit(x_train, y_train)
 svc_predictions = best_svc.predict(x_test)
 
 
-joblib.dump(best_svc, 'D:\\group_projects\\Sign-machine-learning\\main\\source\\models\\svm_3_2_5_01.joblib')
+joblib.dump(best_svc, 'D:\\group_projects\\Sign-machine-learning\\main\\source\\models\\svm_1.joblib')
 # best_svc = joblib.load('D:\\group_projects\\Sign-machine-learning\\main\\source\\models\\svm_1.joblib')
 
 print('Classification report: ', classification_report(y_test, svc_predictions), '\n')
